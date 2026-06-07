779ceb3deb34e528f0fc5563d16c130041178fee
septiandwica <septiandwica03@gmail.com>
2026-06-08 06:42:45 +0700

    feat: add reset.php to clear user logs and KYC attempts for site administrators

---
+++ reset.php
@@ -0,0 +1,11 @@
+<?php
+require_once(__DIR__ . '/../../config.php');
+require_login();
+if (is_siteadmin()) {
+    global $DB;
+    $DB->delete_records('local_netrago_logs', ['userid' => $USER->id]);
+    $DB->delete_records('local_netrago_kyc_attempts', ['userid' => $USER->id]);
+    echo "<h1>All your NetraGo strikes and KYC attempts have been cleared!</h1><p>You can now close this tab and try the quiz again.</p>";
+} else {
+    echo "Must be admin.";
+}
